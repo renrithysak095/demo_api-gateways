47f6b861bc4879a75843d0a706b5358bc0ec43c2
IamSak <renrithysak095@gmail.com>
2023-09-19 13:53:55 +0700

    init

---
+++ api-gateway-config/src/main/java/com/example/apigatewayconfig/ApiGatewayConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayConfigApplication.class, args);
+    }
+
+}
---
+++ api-gateway-config/src/test/java/com/example/apigatewayconfig/ApiGatewayConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
